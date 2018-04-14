4b576202e0fb1b4630b631d2d0d09bbdc0503d5e
EC2 Default User <ec2-user@ip-172-31-15-138.us-east-2.compute.internal>
2018-04-14 11:43:51 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+  
+  resources :microposts, only: [:create, :destroy]
 end
 
 
